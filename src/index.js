0ea5f1280cb34615ca34485cf08de624d134411f
Peppy8651 <73673342+Peppy8651@users.noreply.github.com>
2021-01-07 14:07:31 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ module.exports = class {
         const conf = this.options[source];
             let connection = await nodeFetch(conf.search.url+search,{headers:{'User-Agent':this.options.useragent},timeout:this.options.timeout});
             let doc = await connection.text();
-            console.log(doc);
             let $ = cheerio.load(doc);
             const urlElement = $(conf.search.select).first();
+            console.log(urlElement);
             const url = urlElement.prop('href');
             connection = await nodeFetch(url,{headers:{'User-Agent':this.options.useragent},timeout:this.options.timeout});
             doc = await connection.text();
